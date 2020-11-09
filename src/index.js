e07819322c398fbb3754ee7eeac2ada96154ec4f
chaikovskyit <chaikovskyit@gmail.com>
2020-11-09 15:10:59 +0200

    use simple fetch request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello DartWaider');
+
+
+// Функція яка повертає promise
+fetch('https://swapi.dev/api/people/1/')
+  // коли вона виконається ми тримаємо обєкт "response" тобто результат по запиту
+  .then((res) => {
+    return res.json()
+  })
+  // тіло результату
+  .then((body) => {
+    console.log(body);
+  })
+
+
