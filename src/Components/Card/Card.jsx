b3ccf3fbbc210d9bc1a454d1a944241b45e493d9
chaikovskyit <chaikovskyit@gmail.com>
2020-11-12 17:34:42 +0200

    base code

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Card.css'
+
+const Card = () => {
+  return(
+    <div className="Card">
+      <img src="https://pbs.twimg.com/profile_images/1167221863103074305/Ziap6jxO_400x400.png" alt=""/>
+      <span>Name: Yoda</span>
+      <hr/>
+      <span>Birth year: 19BBY</span>
+  </div> 
+  )
+}
+
+export default Card
---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Search.css'
+
+const Search = () => {
+  return(
+    <div className="Search">
+      <input type="text" placeholder="Whom you search ?"/>
+    </div>
+  )
+}
+
+export default Search
